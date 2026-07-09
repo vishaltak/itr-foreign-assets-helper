dff7ce48510e99764237d7a6b52cce7ebd4b32e2
Vishal Tak <vtak@gitlab.com>
2026-07-09 23:26:48 +0530

    Remove Python implementation
    The project is fully ported to Go. Remove the original Python package and
    its tooling (poetry/pyproject) and the Python-specific gitignore entries.

--- itr_foreign_assets_helper/itr.py
+++
@@ -1,9 +0,0 @@
-class ScheduleRecord:
-
-    def __repr__(self) -> str:
-        attrs = ', '.join(f'{key}={value!r}' for key, value in vars(self).items())
-        return f'{self.__class__.__name__}({attrs})'
-    
-    def __str__(self) -> str:
-        attrs = ', '.join(f'{key}={value}' for key, value in vars(self).items())
-        return f'{self.__class__.__name__}({attrs})'
